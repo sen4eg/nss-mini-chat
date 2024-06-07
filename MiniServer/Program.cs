c2949e83665c3e4873101debc713d9c0505b668e
Arso <mr.sen4eg@gmail.com>
2024-06-07 20:50:09 +0200

    Reconfiguring git to ignore temporary IDE project files

--- MiniServer/Program.cs
+++ MiniServer/Program.cs
@@ -16,7 +16,6 @@ namespace MiniServer
             // Configure the HTTP request pipeline.
             app.MapGrpcService<GreeterService>();
             app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
-
             app.Run();
         }
     }
